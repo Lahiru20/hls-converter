430e20313372c868879f418c22b425628254d43e
Lahiru20 <learn.kklahirumatara@gmail.com>
2025-04-22 09:57:45 +0530

    hls-multipartfile methods implementation

--- src/main/java/org/lahirurashmika/converter/HLSConverterMultipartFile.java
+++ src/main/java/org/lahirurashmika/converter/HLSConverterMultipartFile.java
@@ -13,80 +13,91 @@ import java.util.Objects;
 
 public class HLSConverterMultipartFile {
 
-    // Convert MultipartFile to HLS (returns array of MultipartFiles)
-    public MultipartFile[] convertToHlsMultipartFile(
-            MultipartFile inputFile,
-            String outputFileName,
-            OutputType outputType,
-            int segmentDuration,
-            int hlsListSize) throws Exception {
-
-        // 1. Save input file to temp directory
-        Path tempDir = Files.createTempDirectory("hls-input-");
-        Path inputPath = tempDir.resolve(Objects.requireNonNull(inputFile.getOriginalFilename()));
-        inputFile.transferTo(inputPath);
-
-        // 2. Convert to HLS
-        String[] generatedFiles = new HLSConverter().convertToHls(
-                inputPath.toString(),
-                tempDir.toString(),
-                outputFileName,
-                outputType,
-                segmentDuration,
-                hlsListSize,
-                null,
-                null
-        );
-
-        // 3. Convert generated files to MultipartFile[]
-        List<MultipartFile> result = new ArrayList<>();
-        for (String filePath : generatedFiles) {
-            File file = new File(filePath);
-            result.add(new MockMultipartFile(
-                    file.getName(),
-                    file.getName(),
-                    "application/octet-stream",
-                    Files.readAllBytes(file.toPath())
-            ));
+        // Convert MultipartFile to HLS (returns array of MultipartFiles)
+        public MultipartFile[] convertToHlsMultipartFile(
+                        MultipartFile inputFile,
+                        String outputFileName,
+                        OutputType outputType) throws Exception {
+                return convertToHlsMultipartFile(inputFile, outputFileName, outputType, 10, 5);
         }
 
-        // 4. Cleanup temp files
-        FileUtils.deleteDirectory(tempDir.toFile());
+        public MultipartFile[] convertToHlsMultipartFile(
+                        MultipartFile inputFile,
+                        String outputFileName,
+                        OutputType outputType,
+                        int segmentDuration) throws Exception {
+                return convertToHlsMultipartFile(inputFile, outputFileName, outputType, segmentDuration, 5);
+        }
 
-        return result.toArray(new MultipartFile[0]);
-    }
+        public MultipartFile[] convertToHlsMultipartFile(
+                        MultipartFile inputFile,
+                        String outputFileName,
+                        OutputType outputType,
+                        int segmentDuration,
+                        int hlsListSize) throws Exception {
 
-    // Convert HLS (MultipartFile) to output format
-    public MultipartFile convertFromHlsMultipartFile(
-            MultipartFile hlsFile,
-            String outputFileName,
-            OutputType outputType) throws Exception {
+                // 1. Save input file to temp directory
+                Path tempDir = Files.createTempDirectory("hls-input-");
+                Path inputPath = tempDir.resolve(Objects.requireNonNull(inputFile.getOriginalFilename()));
+                inputFile.transferTo(inputPath);
 
-        // 1. Save HLS file to temp directory
-        Path tempDir = Files.createTempDirectory("hls-output-");
-        Path m3u8Path = tempDir.resolve(Objects.requireNonNull(hlsFile.getOriginalFilename()));
-        Files.write(m3u8Path, hlsFile.getBytes());
+                // 2. Convert to HLS
+                String[] generatedFiles = new HLSConverter().convertToHls(
+                                inputPath.toString(),
+                                tempDir.toString(),
+                                outputFileName,
+                                outputType,
+                                segmentDuration,
+                                hlsListSize,
+                                null,
+                                null);
 
-        // 2. Convert from HLS
-        String outputFilePath = new HLSConverter().convertFromHls(
-                m3u8Path.toString(),
-                tempDir.toString(),
-                outputFileName,
-                outputType
-        );
+                // 3. Convert generated files to MultipartFile[]
+                List<MultipartFile> result = new ArrayList<>();
+                for (String filePath : generatedFiles) {
+                        File file = new File(filePath);
+                        result.add(new MockMultipartFile(
+                                        file.getName(),
+                                        file.getName(),
+                                        "application/octet-stream",
+                                        Files.readAllBytes(file.toPath())));
+                }
 
-        // 3. Read output file into MultipartFile
-        File outputFile = new File(outputFilePath);
-        MultipartFile result = new MockMultipartFile(
-                outputFile.getName(),
-                outputFile.getName(),
-                "application/octet-stream",
-                Files.readAllBytes(outputFile.toPath())
-        );
+                // 4. Cleanup temp files
+                FileUtils.deleteDirectory(tempDir.toFile());
 
-        // 4. Cleanup
-        FileUtils.deleteDirectory(tempDir.toFile());
-        return result;
-    }
+                return result.toArray(new MultipartFile[0]);
+        }
+
+        // Convert HLS (MultipartFile) to output format
+        public MultipartFile convertFromHlsMultipartFile(
+                        MultipartFile hlsFile,
+                        String outputFileName,
+                        OutputType outputType) throws Exception {
+
+                // 1. Save HLS file to temp directory
+                Path tempDir = Files.createTempDirectory("hls-output-");
+                Path m3u8Path = tempDir.resolve(Objects.requireNonNull(hlsFile.getOriginalFilename()));
+                Files.write(m3u8Path, hlsFile.getBytes());
+
+                // 2. Convert from HLS
+                String outputFilePath = new HLSConverter().convertFromHls(
+                                m3u8Path.toString(),
+                                tempDir.toString(),
+                                outputFileName,
+                                outputType);
+
+                // 3. Read output file into MultipartFile
+                File outputFile = new File(outputFilePath);
+                MultipartFile result = new MockMultipartFile(
+                                outputFile.getName(),
+                                outputFile.getName(),
+                                "application/octet-stream",
+                                Files.readAllBytes(outputFile.toPath()));
+
+                // 4. Cleanup
+                FileUtils.deleteDirectory(tempDir.toFile());
+                return result;
+        }
 
 }
